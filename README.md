41e9e1dd1c76c93dd7be465b619b4e32d70b1eef
Adam Kubica <xcdr@kaizen-step.com>
2020-01-23 15:06:47 +0100

    Renamed repository and tags names

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-PostgreSQL Docker images (Alpine)
-=================================
+# PostgreSQL Docker images (Alpine)
 
 PostgreSQL with pl_PL.UTF-8 locale enabled using the official Docker base image.
 
--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 set -e
 
 for ver in 10 11 12; do
-    docker build --build-arg PG_VERSION=$ver --rm -t xcdr/docker-postgres:${ver}-pl .
-    docker push xcdr/docker-postgres:${ver}-pl
+    docker build --build-arg PG_VERSION=$ver --rm -t xcdr/postgres:${ver} .
+    docker push xcdr/postgres:${ver}
 done
